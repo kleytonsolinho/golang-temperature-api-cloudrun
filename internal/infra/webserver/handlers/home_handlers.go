93c0d8322d70775f7c7b92a6e62ac62411705afa
Kleyton Solinho <kleytonsolinho@gmail.com>
2024-02-04 13:04:26 -0300

    feat: update home handler to include link to specific CEP

--- internal/infra/webserver/handlers/home_handlers.go
+++ internal/infra/webserver/handlers/home_handlers.go
@@ -4,5 +4,5 @@ import "net/http"
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("<h1>Welcome to the temperature API</h1>"))
+	w.Write([]byte("<div><h1>Welcome to the temperature API</h1><a href='/cep/01001000'>Click Here <p style='color:red;'>/cep/01001000</p></a></div>"))
 }
